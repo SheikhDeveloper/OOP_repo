f0c35313409a2dd094cf77e94d675afd2769b545
Sheikh <tvemperror00@gmail.com>
2024-09-05 17:42:54 +0300

    Implement encoding functions and modify header file

--- 1/encoder.cc
+++ 1/encoder.cc
@@ -1,22 +1,49 @@
 #include "encoder.hpp"
 
 #include <string>
-#include <cstring>
-//#include <algorithm>
+#include <sstream>
+#include <algorithm>
 
-using std::string;
 using std::pair;
+using std::string;
 
-struct TEncoder {
+struct _tencoder {
     char *encode(const char *input) {
-        char *encoded_input = new char[3 * strlen(input)];
+        std::stringstream encoding_stream;
+        for (char c = *input; c != 0; c++) {
+            if (!isalpha(c) && !isdigit(c) && c != '-' && c != '_') {
+                encoding_stream << "%" << std::hex << c;
+            }
+            else encoding_stream << std::dec << c;
+        }
+        string encoded_inp_str = encoding_stream.str();
+        char *encoded_input = encoded_inp_str.data();
         return encoded_input;
     }    
-    pair<char *, size_t> *encode(const char *input, size_t input_size) {
-
+    pair<char *, size_t> encode(const char *input, size_t input_size) {
+        std::stringstream encoding_stream;
+        for (size_t i= 0; i < input_size; i++) {
+            if (!isalpha(input[i]) && !isdigit(input[i]) && input[i] != '-' && input[i] != '_') {
+                encoding_stream << "%" << std::hex << input[i];
+            }
+            else encoding_stream << std::dec << input[i];
+        }
+        string encoded_inp_str = encoding_stream.str();
+        size_t encoded_inp_size = encoded_inp_str.size();
+        char *encoded_input = new char[encoded_inp_size];
+        std::copy_if(encoded_inp_str.begin(), encoded_inp_str.end(), encoded_input, 
+                     [](char x) { return x != 0; });
+        return {encoded_input, encoded_inp_size};
     }
     string encode(string input) {
-
+        std::stringstream encoding_stream;
+        for (auto c : input) {
+            if (!isalpha(c) && !isdigit(c) && c != '-' && c != '_') {
+                encoding_stream << "%" << std::hex << c;
+            }
+            else encoding_stream << std::dec << c;
+        }
+        return encoding_stream.str();
     }
     char *decode(const char *input) {
 
@@ -27,4 +54,5 @@ struct TEncoder {
     string decode(string input) {
 
     }
+
 };
--- 1/encoder.hpp
+++ 1/encoder.hpp
@@ -1,6 +1,6 @@
 #ifndef LAB1_ENCODER_HPP
 #define LAB1_ENCODER_HPP
 
-struct Tencoder;
+typedef struct _tencoder TEncoder;
 
 #endif
