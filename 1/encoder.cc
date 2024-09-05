1bf516a350116d4758f54534058181a95edc356f
Sheikh <tvemperror00@gmail.com>
2024-09-05 19:47:46 +0300

    Implement decoding functions

--- 1/encoder.cc
+++ 1/encoder.cc
@@ -3,18 +3,20 @@
 #include <string>
 #include <sstream>
 #include <algorithm>
+#include <regex>
 
 using std::pair;
 using std::string;
+using std::regex;
 
 struct _tencoder {
     char *encode(const char *input) {
         std::stringstream encoding_stream;
-        for (char c = *input; c != 0; c++) {
-            if (!isalpha(c) && !isdigit(c) && c != '-' && c != '_') {
-                encoding_stream << "%" << std::hex << c;
+        for (const char *c = input; *c != 0; c++) {
+            if (!isalpha(*c) && !isdigit(*c) && *c != '-' && *c != '_') {
+                encoding_stream << "%" << std::hex << *c;
             }
-            else encoding_stream << std::dec << c;
+            else encoding_stream << std::dec << *c;
         }
         string encoded_inp_str = encoding_stream.str();
         char *encoded_input = encoded_inp_str.data();
@@ -46,13 +48,107 @@ struct _tencoder {
         return encoding_stream.str();
     }
     char *decode(const char *input) {
-
+        std::stringstream decoded_input;
+        size_t cur_state = 0;
+        std::stringstream hex_string;
+        for (const char *c = input; *c != 0; c++) {
+            if (cur_state == 0) {
+                if (*c == '%') {
+                    cur_state = 1;
+                }
+                else decoded_input << *c;
+            }
+            else if (cur_state == 1) {
+                if ('0' < *c && *c < '9') {
+                    cur_state = 2;
+                    hex_string << *c;
+                }
+                else {
+                    cur_state = 0;
+                    hex_string.flush();
+                }
+            }
+            else if (cur_state == 2) {
+                if ('0' < *c && *c < '9') {
+                    hex_string << *c;
+                    decoded_input << std::dec << hex_string.str();
+                }
+                hex_string.flush();
+                cur_state = 0;
+            }
+        }
+        string result_str = decoded_input.str();
+        char *result = result_str.data();
+        return result;
     }    
-    pair<const char *, size_t> *decode(const char *input, size_t input_size) {
-
+    pair<const char *, size_t> decode(const char *input, size_t input_size) {
+        std::stringstream decoded_input;
+        size_t cur_state = 0;
+        std::stringstream hex_string;
+        for (size_t i = 0; i < input_size; i++) {
+            if (cur_state == 0) {
+                if (input[i] == '%') {
+                    cur_state = 1;
+                }
+                else decoded_input << input[i];
+            }
+            else if (cur_state == 1) {
+                if ('0' < input[i] && input[i] < '9') {
+                    cur_state = 2;
+                    hex_string << input[i];
+                }
+                else {
+                    cur_state = 0;
+                    hex_string.flush();
+                }
+            }
+            else if (cur_state == 2) {
+                if ('0' < input[i] && input[i] < '9') {
+                    hex_string << input[i];
+                    decoded_input << std::dec << hex_string.str();
+                }
+                hex_string.flush();
+                cur_state = 0;
+            }
+        }
+        string result_str = decoded_input.str();
+        size_t result_size = result_str.size();
+        char *result;
+        std::copy_if(result_str.begin(), result_str.end(), result,
+                [](char x) { return x != 0; } );
+        return {result, result_size};
     }
     string decode(string input) {
-
+        std::stringstream decoded_input;
+        size_t cur_state = 0;
+        std::stringstream hex_string;
+        for (auto c : input) {
+            if (cur_state == 0) {
+                if (c == '%') {
+                    cur_state = 1;
+                }
+                else decoded_input << c;
+            }
+            else if (cur_state == 1) {
+                if ('0' < c && c < '9') {
+                    cur_state = 2;
+                    hex_string << c;
+                }
+                else {
+                    cur_state = 0;
+                    hex_string.flush();
+                }
+            }
+            else if (cur_state == 2) {
+                if ('0' < c && c < '9') {
+                    hex_string << c;
+                    decoded_input << std::dec << hex_string.str();
+                }
+                hex_string.flush();
+                cur_state = 0;
+            }
+        }
+        return decoded_input.str();
     }
 
 };
