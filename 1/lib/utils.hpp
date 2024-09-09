c71310a0a8a749610fcbf1b421f529cfbf16cf43
Sheikh <tvemperror00@gmail.com>
2024-09-09 20:15:31 +0300

    Moved template functions from source to header. Fix CMakeLists.txt

--- 1/lib/utils.hpp
+++ 1/lib/utils.hpp
@@ -1,6 +1,21 @@
 #ifndef LAB1_LIB_UTILS_HPP
 #define LAB1_LIB_UTILS_HPP
 
-template<typename T> bool valid_input(T &variable);
+#include <iostream>
+#include <limits>
+
+template<typename T> bool valid_input(T &variable) {
+    std::cin >> variable;
+    if (std::cin.eof()) return 1;
+    while (std::cin.fail()) {
+        if (std::cin.eof()) return EOF;
+        std::cout << "Invalid input." << std::endl;
+        std::cout << "Try again: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin >> variable;
+    }
+    return 0;
+}
 
 #endif
