2ee79ba4241c30c4164821674df887852679a5a4
Sheikh <tvemperror00@gmail.com>
2024-09-29 21:41:27 +0300

    Implemented remaining constructors, getters, setters, operator+. Add operator>> and operator* to .h file

--- 2/lib/cocktail.cc
+++ 2/lib/cocktail.cc
@@ -2,14 +2,49 @@
 
 #include <string>
 
-Cocktail::Cocktail() {
-    this->name = "";
-    this->alcohol_percentage = 0.;
-    this->volume = 0.;
+TCocktail::TCocktail() {
+    name = L"";
+    alcohol_percentage = 0.;
+    volume = 0.;
 }
 
-Cocktail::Cocktail(std::string name, double alcohol_percentage, double volume) {
-    this->name = name;
-    this->alcohol_percentage = alcohol_percentage;
-    this->volume = volume;
+TCocktail::TCocktail(std::wstring custom_name, double alc_percentage, double vol) {
+    name = custom_name;
+    alcohol_percentage = alc_percentage;
+    volume = vol;
 }
+
+TCocktail::TCocktail(double vol) {
+    name = L"Water";
+    alcohol_percentage = 0.;
+    volume = vol;
+}
+
+const std::wstring &TCocktail::getName() const {
+    return name;
+}
+
+double TCocktail::getAlcoholPercentage() const {
+    return alcohol_percentage;
+}
+
+double TCocktail::getVolume() const {
+    return volume;
+}
+
+void TCocktail::setName(const std::wstring &custom_name) {
+    name = custom_name;
+}
+
+void TCocktail::setAlcoholPercentage(double alc_percentage) {
+    alcohol_percentage = alc_percentage;
+}
+
+void TCocktail::setVolume(double vol) {
+    volume = vol;
+}
+
+TCocktail TCocktail::operator+(const TCocktail &cocktail2) const {
+    return TCocktail(name + L" and " + cocktail2.name, alcohol_percentage + cocktail2.alcohol_percentage, volume + cocktail2.volume);
+}
+
--- 2/lib/cocktail.h
+++ 2/lib/cocktail.h
@@ -1,17 +1,29 @@
-#ifndef LAB2_COCKTAIL_H
-#define LAB2_COCKTAIL_H
+#ifndef LAB2_LIB_COCKTAIL_H
+#define LAB2_LIB_COCKTAIL_H
 
 #include <string>
 
-class Cocktail
+class TCocktail
 {
 private:
-    std::string name;
+    std::wstring name;
     double alcohol_percentage;
     double volume;
 public:
-    Cocktail();
-    Cocktail(std::string name, double alchol_percentage, double volume);
+    TCocktail();
+    TCocktail(std::wstring name, double alchol_percentage, double volume);
+    TCocktail(double volume);
+
+    const std::wstring &getName() const;
+    double getAlcoholPercentage() const;
+    double getVolume() const;
+
+    void setName(const std::wstring &name);
+    void setAlcoholPercentage(double alchol_percentage);
+    void setVolume(double volume);
+    TCocktail operator+(const TCocktail &cocktail2) const;
+    void operator>>(const TCocktail &cocktail2) const;
+    TCocktail operator*(const double &multiplier) const;
 };
 
 #endif //LAB2_COCKTAIL_H
