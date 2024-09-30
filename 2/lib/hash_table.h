2694a8c19bcdb6137001fe79ec015f8c420e07a0
Sheikh <tvemperror00@gmail.com>
2024-09-30 22:37:02 +0300

    Add docstring to hash table

--- 2/lib/hash_table.h
+++ 2/lib/hash_table.h
@@ -5,37 +5,111 @@
 #include <functional>
 #include <stdexcept>
 
+/**
+ * Hash function for a given type.
+ *
+ * @tparam T Type to hash
+ * @param key Value to hash
+ * @return Hashed value
+ */
 template<typename T>
 size_t hash(const T &key) {
     return std::hash<T>{}(key);
 }
 
+/**
+ * Hash table class.
+ *
+ * @tparam T1 Key type
+ * @tparam T2 Value type
+ */
 template<class T1, class T2> 
 class THashTable {
 public:
+    /**
+     * Node structure for the hash table.
+     */
     struct Node {
+        /**
+         * Key of the node.
+         */
         T1 key;
+        /**
+         * Value of the node.
+         */
         T2 value;
+        /**
+         * Whether the node is taken.
+         */
         bool is_taken = false;
+        /**
+         * Whether the node is the last node.
+         */
         bool is_last = false;
+        /**
+         * Previous node with ths same hash index.
+         */
         size_t prev;
+        /**
+         * Next node with the same hash index.
+         */
         size_t next;
 
+        /**
+         * Default constructor of the node.
+         */
         Node() = default;
+
+        /**
+         * Constructor with key, value, and indices.
+         *
+         * @param key Key of the node
+         * @param value Value of the node
+         * @param is_taken Whether the node is taken
+         * @param prev Previous node with the same hash index
+         * @param next Next node with the same hash index
+         * @param is_last Whether the node is the last node
+         */
         Node(const T1 &key, const T2 &value, bool is_taken, size_t prev, size_t next, bool is_last) : key(key), value(value), is_taken(is_taken), prev(prev), next(next), is_last(is_last) {}
     };
 
+    /**
+     * Iterator class for the hash table.
+     */
     class Iterator {
     public:
+        /**
+         * Returns the current node.
+         *
+         * @return Current node
+         */
         virtual Node &operator*() const {
             return *ptr;
         }
+        /**
+         * Checks if two iterators are equal.
+         *
+         * @param other Iterator to compare with
+         * @return Whether the iterators are equal
+         */
         virtual bool operator==(const Iterator &other) const {
             return ptr == other.ptr;
         }
+        /**
+         * Checks if two iterators are not equal.
+         *
+         * @param other Iterator to compare with
+         * @return Whether the iterators are not equal
+         */
         virtual bool operator!=(const Iterator &other) const {
             return ptr != other.ptr;
         }
+        /**
+         * Increments the iterator to the next taken node.
+         * If the iterator is at the end, it stays there.
+         *
+         * @return Reference to the iterator
+         */
         virtual Iterator &operator++() {
             ++ptr;
             while (!(*ptr).is_taken && !(*ptr).is_last) {
@@ -44,18 +118,40 @@ public:
             return *this;
         }
 
+        /**
+         * Iterator destructor.
+         */
         virtual ~Iterator() = default;
 
+        /**
+         * Iterator constructor with a node pointer.
+         *
+         * @param init_ptr Node pointer
+         */
         Iterator(Node *init_ptr) : ptr(init_ptr) {}
 
     private:
+        /**
+         * Node pointer.
+         */
         Node *ptr = nullptr;
     };
 
+    /**
+     * Returns a reference to the value associated with the given key.
+     *
+     * @param key Key to find
+     * @return Reference to the value
+     */
     virtual T2 &operator[](const T1 &key) {
         return (*find(key)).value;
     }
 
+    /**
+     * Assigns the contents of another hash table to this hash table.
+     *
+     * @param other Hash table to assign from
+     */
     void operator=(THashTable &other) {
         table = new Node[other.capacity_];
         capacity_ = other.capacity_;
@@ -65,6 +161,11 @@ public:
         }        
     }
 
+    /**
+     * Assigns the contents of another const hash table to this hash table.
+     *
+     * @param other Hash table to assign from
+     */
     void operator=(const THashTable &other) {
         table = new Node[other.capacity_];
         capacity_ = other.capacity_;
@@ -74,7 +175,15 @@ public:
         }        
     }
     
+    /**
+     * Default constructor.
+     */
     THashTable() = default;
+    /**
+     * Constructor with a capacity.
+     *
+     * @param capacity Capacity of the hash table
+     */
     THashTable(size_t capacity) {
         table = new Node[capacity];
         capacity_ = capacity;
@@ -82,6 +191,11 @@ public:
             table[i].next = table[i].prev = i;
         }
     }
+    /**
+     * Copy constructor.
+     *
+     * @param other Hash table to copy from
+     */
     THashTable(THashTable &other) {
         table = new Node[other.capacity_];
         capacity_ = other.capacity_;
@@ -90,18 +204,46 @@ public:
         }
         size_ = other.size_;
     }
+
+    /**
+     * Constructor with a node array, size, and capacity.
+     *
+     * @param table Node array
+     * @param size Size of the hash table
+     * @param capacity Capacity of the hash table
+     */
     THashTable(Node *table, size_t size, size_t capacity) : table(table), size_(size), capacity_(capacity) {}
+
+    /**
+     * Move constructor.
+     * 
+     * Constructs a new THashTable object by moving the contents of another THashTable object.
+     * 
+     * @param other The THashTable object to move from.
+     */
     THashTable(THashTable &&other) : table(other.table), size_(other.size_), capacity_(other.capacity_) {
         other.table = nullptr;
         other.size_ = 0;
         other.capacity_ = 0;
     }
 
+    /**
+     * Destructor.
+     * 
+     * Releases the memory allocated for the hash table.
+     */
     virtual ~THashTable() {
         delete[] table;
         table = nullptr;
     }
 
+    /**
+     * Returns an iterator pointing to the beginning of the hash table.
+     * 
+     * The iterator points to the first element in the hash table, or to the end of the hash table if it is empty.
+     * 
+     * @return An iterator pointing to the beginning of the hash table.
+     */
     virtual Iterator begin() {
         for (size_t i = 0; i < capacity_; ++i) {
             if (table[i].is_taken) {
@@ -110,9 +252,26 @@ public:
         }
         return end();
     }
+
+    /**
+     * Returns an iterator pointing to the end of the hash table.
+     * 
+     * The iterator points to a position one past the last element in the hash table.
+     * 
+     * @return An iterator pointing to the end of the hash table.
+     */
     virtual Iterator end() {
         return Iterator(table + capacity_);
     }
+
+    /**
+     * Finds an element in the hash table with the given key.
+     * 
+     * If the key is found, the iterator points to the corresponding element. Otherwise, the iterator points to the end of the hash table.
+     * 
+     * @param key The key to search for.
+     * @return An iterator pointing to the element with the given key, or to the end of the hash table if the key is not found.
+     */
     virtual Iterator find(const T1 &key) {
         auto index = hash(key) % capacity_;
         while (table[index].key != key || !table[index].is_taken) {
@@ -123,6 +282,15 @@ public:
         }
         return Iterator(table + index);
     }
+
+    /**
+     * Inserts a new element into the hash table.
+     * 
+     * If the hash table is full, it is resized to accommodate the new element.
+     * 
+     * @param key The key of the new element.
+     * @param value The value of the new element.
+     */
     virtual void insert(const T1 &key, const T2 &value) {
         if (size_ == capacity_) {
             resize();
@@ -140,6 +308,14 @@ public:
         table[index] = Node(key, value, true, prev_index, next_index, is_last);
         ++size_;
     }
+
+    /**
+     * Removes an element from the hash table with the given key.
+     * 
+     * If the key is found, the element is removed and the size of the hash table is decreased.
+     * 
+     * @param key The key of the element to remove.
+     */
     virtual void remove(const T1 &key) {
         auto hash_index = hash(key) % capacity_;
         auto element = find(key);
@@ -162,15 +338,40 @@ public:
         }
         --size_;
     }
+
+    /**
+     * Checks if the hash table contains an element with the given key.
+     * 
+     * @param key The key to search for.
+     * @return True if the key is found, false otherwise.
+     */
     virtual bool contains(const T1 &key) {
         return find(key) != end();
     }
+
+    /**
+     * Returns the number of elements in the hash table.
+     * 
+     * @return The size of the hash table.
+     */
     virtual size_t size() const {
         return size_;
     }
+
+    /**
+     * Returns the capacity of the hash table.
+     * 
+     * @return The capacity of the hash table.
+     */
     virtual size_t capacity() const {
         return capacity_;
     }
+
+    /**
+     * Clears the hash table.
+     * 
+     * All elements are removed and the size and capacity of the hash table are reset to 0.
+     */
     virtual void clear() {
         size_ = 0;
         capacity_ = 0;
@@ -178,10 +379,34 @@ public:
         table = nullptr;
     }
 private:
+    /**
+     * The table of nodes in the hash table.
+     * 
+     * Each node represents an element in the hash table, and contains a key, value, and pointers to the next and previous nodes.
+     */
     Node *table = nullptr;
+
+    /**
+     * The capacity of the hash table.
+     * 
+     * The capacity is the maximum number of elements that the hash table can hold.
+     */
     size_t capacity_ = 0;
+
+    /**
+     * The current size of the hash table.
+     * 
+     * The size is the number of elements currently stored in the hash table.
+     */
     size_t size_ = 0;
 
+    /**
+     * Resizes the hash table to twice its current capacity.
+     * 
+     * All elements are reinserted into the new hash table.
+     * 
+     * This function is called when the hash table is full and a new element needs to be inserted.
+     */
     void resize() {
         capacity_ *= 2;
         auto *new_table = new Node[capacity_];
