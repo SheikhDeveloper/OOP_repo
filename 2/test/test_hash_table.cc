2bc7c24b88dc93bf30342704e1812e9b394e147d
Sheikh <tvemperror00@gmail.com>
2024-10-12 21:04:23 +0300

    Add TestStruct for HashTable. Change tests accordingly

--- 2/test/test_hash_table.cc
+++ 2/test/test_hash_table.cc
@@ -1,30 +1,73 @@
 #include "gtest/gtest.h"
 #include "../lib/hash_table.h"
 
+struct TestStruct {
+    int *value_;
+    TestStruct() : value_(new int(0)) {}
+    TestStruct(int value) : value_(new int(value)) {}
+    TestStruct(const TestStruct &other) : value_(new int(*other.value_)) {}
+    TestStruct(TestStruct &&other) : value_(other.value_) {
+        other.value_ = nullptr;
+    }
+
+    TestStruct &operator=(const TestStruct &other) {
+        if (this == &other) {
+            return *this;
+        }
+        delete value_;
+        value_ = new int(*other.value_);
+        return *this;
+    }
+
+    TestStruct &operator=(TestStruct &&other) {
+        if (this == &other) {
+            return *this;
+        }
+        delete value_;
+        value_ = other.value_;
+        other.value_ = nullptr;
+        return *this;
+    }
+    TestStruct &operator=(int value) {
+        delete value_;
+        value_ = new int(value);
+        return *this;
+    }
+    ~TestStruct() {
+        delete value_;
+    }
+
+    int operator*() {
+        return *value_;
+    }
+
+
+};
+
 TEST(THashTableTest, DefaultConstructor) {
-    THashTable<int, int> table;
+    THashTable<int, TestStruct> table;
     EXPECT_EQ(table.size(), 0);
     EXPECT_EQ(table.capacity(), 0);
 }
 
 TEST(THashTableTest, ParameterizedConstructor) {
-    THashTable<int, int> table(10);
+    THashTable<int, TestStruct> table(10);
     EXPECT_EQ(table.size(), 0);
     EXPECT_EQ(table.capacity(), 10);
 }
 
 TEST(THashTableTest, Insert) {
-    THashTable<int, int> table(10);
+    THashTable<int, TestStruct> table(10);
     table.insert(1, 1);
     table.insert(2, 2);
     EXPECT_EQ(table.size(), 2);
     EXPECT_EQ(table.capacity(), 10);
-    EXPECT_EQ((*(table.find(1))).value, 1);
-    EXPECT_EQ((*(table.find(2))).value, 2);
+    EXPECT_EQ(*((*(table.find(1))).value_), 1);
+    EXPECT_EQ(*((*(table.find(2))).value_), 2);
 }
 
 TEST(THashTableTest, RemoveExistingKey_FirstElement) {
-    THashTable<int, int> hashTable(10);
+    THashTable<int, TestStruct> hashTable(10);
     hashTable.insert(1, 1);
     hashTable.insert(2, 2);
     hashTable.insert(3, 3);
@@ -36,7 +79,7 @@ TEST(THashTableTest, RemoveExistingKey_FirstElement) {
 }
 
 TEST(THashTableTest, RemoveExistingKey_NotFirstElement) {
-    THashTable<int, int> hashTable(10);
+    THashTable<int, TestStruct> hashTable(10);
     hashTable.insert(1, 1);
     hashTable.insert(2, 2);
     hashTable.insert(3, 3);
@@ -48,7 +91,7 @@ TEST(THashTableTest, RemoveExistingKey_NotFirstElement) {
 }
 
 TEST(THashTableTest, RemoveNonExistingKey) {
-    THashTable<int, int> hashTable(10);
+    THashTable<int, TestStruct> hashTable(10);
     hashTable.insert(1, 1);
     hashTable.insert(2, 2);
 
@@ -57,7 +100,7 @@ TEST(THashTableTest, RemoveNonExistingKey) {
 }
 
 TEST(THashTableTest, Contains) {
-    THashTable<int, int> table(10);
+    THashTable<int, TestStruct> table(10);
     table.insert(1, 1);
     table.insert(2, 2);
     EXPECT_TRUE(table.contains(1));
@@ -66,21 +109,21 @@ TEST(THashTableTest, Contains) {
 }
 
 TEST(THashTableTest, Size) {
-    THashTable<int, int> table(10);
+    THashTable<int, TestStruct> table(10);
     table.insert(1, 1);
     table.insert(2, 2);
     EXPECT_EQ(table.size(), 2);
 }
 
 TEST(THashTableTest, Capacity) {
-    THashTable<int, int> table(10);
+    THashTable<int, TestStruct> table(10);
     table.insert(1, 1);
     table.insert(2, 2);
     EXPECT_EQ(table.capacity(), 10);
 }
 
 TEST(THashTableTest, Clear) {
-    THashTable<int, int> table(10);
+    THashTable<int, TestStruct> table(10);
     table.insert(1, 1);
     table.insert(2, 2);
     table.clear();
@@ -89,7 +132,7 @@ TEST(THashTableTest, Clear) {
 }
 
 TEST(THashTableTest, Resize) {
-    THashTable<int, int> table(2);
+    THashTable<int, TestStruct> table(2);
     table.insert(1, 1);
     table.insert(2, 2);
     table.insert(3, 3);
@@ -98,19 +141,19 @@ TEST(THashTableTest, Resize) {
 }
 
 TEST(THashTableTest, Iterator) {
-    THashTable<int, int> table(10);
+    THashTable<int, TestStruct> table(10);
     table.insert(1, 1);
     table.insert(2, 2);
     auto it = table.begin();
-    EXPECT_EQ((*it).key, 1);
-    EXPECT_EQ((*it).value, 1);
+    EXPECT_EQ((*it).key_, 1);
+    EXPECT_EQ(*((*it).value_), 1);
     ++it;
-    EXPECT_EQ((*it).key, 2);
-    EXPECT_EQ((*it).value, 2);
+    EXPECT_EQ((*it).key_, 2);
+    EXPECT_EQ(*((*it).value_), 2);
 }
 
 TEST(THashTableTest, End) {
-    THashTable<int, int> table(10);
+    THashTable<int, TestStruct> table(10);
     table.insert(1, 1);
     table.insert(2, 2);
     auto it = table.end();
@@ -118,68 +161,68 @@ TEST(THashTableTest, End) {
 }
 
 TEST(THashTableTest, Find) {
-    THashTable<int, int> table(10);
+    THashTable<int, TestStruct> table(10);
     table.insert(1, 1);
     table.insert(2, 2);
     auto it = table.find(1);
-    EXPECT_EQ((*it).key, 1);
-    EXPECT_EQ((*it).value, 1);
+    EXPECT_EQ((*it).key_, 1);
+    EXPECT_EQ(*((*it).value_), 1);
 }
 
 TEST(THashTableTest, OperatorBracket) {
-    THashTable<int, int> table(10);
+    THashTable<int, TestStruct> table(10);
     table.insert(1, 1);
     table.insert(2, 2);
-    EXPECT_EQ(table[1], 1);
-    EXPECT_EQ(table[2], 2);
+    EXPECT_EQ(*(table[1]), 1);
+    EXPECT_EQ(*(table[2]), 2);
 }
 
 TEST(THashTableTest, CopyAssignment) {
     // Create a hash table
-    THashTable<int, int> hashTable{};
+    THashTable<int, TestStruct> hashTable{};
 
     // Insert some elements
     hashTable.insert(-1, 1);
     hashTable.insert(-2, 2);
 
     // Create another hash table
-    THashTable<int, int> anotherHashTable{};
+    THashTable<int, TestStruct> anotherHashTable{};
 
     // Assign the first hash table to the second
     anotherHashTable = hashTable;
 
     // Check if the assignment was successful
     EXPECT_EQ(anotherHashTable.size(), 2);
-    EXPECT_EQ(anotherHashTable[-1], 1);
-    EXPECT_EQ(anotherHashTable[-2], 2);
+    EXPECT_EQ(*(anotherHashTable[-1]), 1);
+    EXPECT_EQ(*(anotherHashTable[-2]), 2);
 
     // Modify the first hash table
     hashTable.insert(-3, 3);
 
     // Check if the second hash table remains unchanged
     EXPECT_EQ(anotherHashTable.size(), 2);
-    EXPECT_EQ(anotherHashTable[-1], 1);
-    EXPECT_EQ(anotherHashTable[-2], 2);
+    EXPECT_EQ(*(anotherHashTable[-1]), 1);
+    EXPECT_EQ(*(anotherHashTable[-2]), 2);
 }
 
 TEST(THashTableTest, MoveAssignment) {
     // Create a hash table
-    THashTable<int, int> hashTable{};
+    THashTable<int, TestStruct> hashTable{};
 
     // Insert some elements
     hashTable.insert(-1, 1);
     hashTable.insert(-2, 2);
 
     // Create another hash table
-    THashTable<int, int> anotherHashTable{};
+    THashTable<int, TestStruct> anotherHashTable{};
 
     // Move assign the first hash table to the second
     anotherHashTable = std::move(hashTable);
 
     // Check if the move assignment was successful
     EXPECT_EQ(anotherHashTable.size(), 2);
-    EXPECT_EQ(anotherHashTable[-1], 1);
-    EXPECT_EQ(anotherHashTable[-2], 2);
+    EXPECT_EQ(*(anotherHashTable[-1]), 1);
+    EXPECT_EQ(*(anotherHashTable[-2]), 2);
 
     // Check if the first hash table is in a valid but unspecified state
     EXPECT_EQ(hashTable.size(), 0);
@@ -187,7 +230,7 @@ TEST(THashTableTest, MoveAssignment) {
 
 TEST(THashTableTest, CopyConstructor) {
     // Create a hash table
-    THashTable<int, int> hashTable{};
+    THashTable<int, TestStruct> hashTable{};
 
     // Insert some elements
     hashTable.insert(-1, 1);
@@ -198,21 +241,21 @@ TEST(THashTableTest, CopyConstructor) {
 
     // Check if the copy was successful
     EXPECT_EQ(anotherHashTable.size(), 2);
-    EXPECT_EQ(anotherHashTable[-1], 1);
-    EXPECT_EQ(anotherHashTable[-2], 2);
+    EXPECT_EQ(*(anotherHashTable[-1]), 1);
+    EXPECT_EQ(*(anotherHashTable[-2]), 2);
 
     // Modify the first hash table
     hashTable.insert(-3, 3);
 
     // Check if the second hash table remains unchanged
     EXPECT_EQ(anotherHashTable.size(), 2);
-    EXPECT_EQ(anotherHashTable[-1], 1);
-    EXPECT_EQ(anotherHashTable[-2], 2);
+    EXPECT_EQ(*(anotherHashTable[-1]), 1);
+    EXPECT_EQ(*(anotherHashTable[-2]), 2);
 }
 
 TEST(THashTableTest, MoveConstructor) {
     // Create a hash table
-    THashTable<int, int> hashTable{};
+    THashTable<int, TestStruct> hashTable{};
 
     // Insert some elements
     hashTable.insert(-1, 1);
@@ -223,8 +266,8 @@ TEST(THashTableTest, MoveConstructor) {
 
     // Check if the move was successful
     EXPECT_EQ(anotherHashTable.size(), 2);
-    EXPECT_EQ(anotherHashTable[-1], 1);
-    EXPECT_EQ(anotherHashTable[-2], 2);
+    EXPECT_EQ(*(anotherHashTable[-1]), 1);
+    EXPECT_EQ(*(anotherHashTable[-2]), 2);
 
     // Check if the first hash table is in a valid but unspecified state
     EXPECT_EQ(hashTable.size(), 0);
